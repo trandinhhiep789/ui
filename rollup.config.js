f2168f236590b42768afce684d0c26d295ccfc37
eliot <trandinhhiep789@gmail.com>
2022-08-02 15:09:21 +0700

    setup storybook

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,9 @@ import nodeResolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
+import { terser } from "rollup-plugin-terser";
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
 const packageJson = require("./package.json");
 
@@ -21,9 +24,12 @@ export default [
       },
     ],
     plugins: [
+      peerDepsExternal(),
       nodeResolve(),
       commonjs(),
       typescript({ tsconfig: "./tsconfig.json" }),
+      postcss(), 
+      terser(),
     ],
   },
   {
@@ -31,5 +37,6 @@ export default [
     input: "dist/esm/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
+    external: [/\.css$/],
   },
 ];
