91421093a5931d9290ac05022fc53ffa3aeca09f
eliot <trandinhhiep789@gmail.com>
2022-08-03 13:20:53 +0700

    fix: warning error tsx file in storybook

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 import "./Button.scss"
 
-export interface ButtonProps {
-  label: string;
+interface ButtonProps {
+  primary?: boolean;
+  size?: 'small' | 'medium' | 'large';
+  label: string
 }
 
-const Button = (props: ButtonProps) => {
-  return <button className="bg-teal-200">{props.label}</button>;
-};
+const defaultBtn:string = "text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
 
-export default Button;
+const alternativeBtn:string = "py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+
+export const Button = ({
+  primary = false,
+  size = 'medium',
+  label,
+  ...props
+}: ButtonProps) => {
+  const mode = primary ? defaultBtn : alternativeBtn;
+  return 
+  (<button
+    type="button"
+    className={mode}
+    {...props}
+  >
+    {label}
+  </button>)
+};
