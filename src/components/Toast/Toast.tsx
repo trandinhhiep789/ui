6ce727c580d2c28c25e577d9bc2af733d15d8bca
eliot <trandinhhiep789@gmail.com>
2022-09-20 10:57:48 +0700

    doing: tim cach export funtion toast

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect, useCallback, ReactNode } from 'react'
 
 import './Toast.css'
 
@@ -12,10 +12,45 @@ interface ToastProps {
   type?: 'success' | 'failure' | 'warning' | 'info'
   /** Vị trí Toast */
   placement?: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right'
+  duration?: number
 }
 
-const Toast = ({ content, type = 'success', placement = 'bottom-right', ...props }: ToastProps) => {
-  const iconType = () => {
+const Toast = ({
+  content = 'success',
+  type = 'success',
+  placement = 'bottom-right',
+  duration = 3000,
+  ...props
+}: ToastProps) => {
+  const [list, setList] = useState<{ id: number; title: string; description: string; icon: ReactNode }[]>([])
+
+  const deleteToast = useCallback(
+    (id: number) => {
+      const toastListItem = list.filter(e => e.id !== id)
+      setList(toastListItem)
+    },
+    [list, setList]
+  )
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (list.length) {
+        deleteToast(list[0].id)
+      }
+    }, duration)
+    return () => {
+      clearInterval(interval)
+    }
+  }, [list, deleteToast])
+
+  let initToastProperties = {
+    id: -1,
+    title: '',
+    description: '',
+    icon: <FontAwesomeIcon icon={faCircleCheck} className="faCircleCheck" />
+  }
+
+  const iconType = (type: string) => {
     switch (type) {
       case 'success':
         return <FontAwesomeIcon icon={faCircleCheck} className="faCircleCheck" />
@@ -30,15 +65,64 @@ const Toast = ({ content, type = 'success', placement = 'bottom-right', ...props
     }
   }
 
+  const showToast = (type: string, content: string) => {
+    switch (type) {
+      case 'success':
+        initToastProperties = {
+          id: list.length + 1,
+          title: 'Success',
+          description: content,
+          icon: iconType(type)
+        }
+        break
+      case 'failure':
+        initToastProperties = {
+          id: list.length + 1,
+          title: 'Failure',
+          description: content,
+          icon: iconType(type)
+        }
+        break
+      case 'info':
+        initToastProperties = {
+          id: list.length + 1,
+          title: 'Info',
+          description: content,
+          icon: iconType(type)
+        }
+        break
+      case 'warning':
+        initToastProperties = {
+          id: list.length + 1,
+          title: 'Warning',
+          description: content,
+          icon: iconType(type)
+        }
+        break
+      default:
+        initToastProperties = {
+          id: -1,
+          title: '',
+          description: '',
+          icon: iconType('success')
+        }
+    }
+    setList(prevList => [...prevList, initToastProperties])
+  }
+
   return (
     <div {...props}>
-      <div className={['toast', type, placement].join(' ')}>
-        <div className="flex w-full">
-          <div className="icon">{iconType()}</div>
-          <div className="contentMessage">
-            <p>{content}</p>
+      <div onClick={() => showToast(type, content)}>123</div>
+
+      <div className={['toast', placement].join(' ')}>
+        {list.map((toast, i) => (
+          <div className={['divToast', type].join(' ')} key={i}>
+            <div className="icon">{toast.icon}</div>
+            <div className="contentMessage">
+              <p>{content}</p>
+            </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   )
